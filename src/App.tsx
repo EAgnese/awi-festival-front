5629f46b6729b38ee9e22a150c63990931f6fcbf
emma-mathieu <emma.mathieu@etu.umontpellier.fr>
2023-02-09 17:58:51 +0100

    route mise en place

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Benevole from './components/Benevole'
+import Zone from './components/Zone'
+import { Outlet } from "react-router-dom";//pour indiquer ou placer le chield component dans app
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>App</h1>
+      <Outlet />
+      <h1>App fin</h1>
     </div>
   )
 }
---
+++ src/components/Zone.tsx
@@ -0,0 +1,10 @@
+
+function Zone(){
+    return(
+        <div>
+            <h1>Bienvenue sur la page des zones</h1>
+        </div>
+    )
+}
+
+export default Zone
