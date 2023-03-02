8546ea136c6a05fe1abc9ebee13d090d53d6a932
EAgnese <agneseeri@gmail.com>
2023-03-02 18:21:17 +0100

    correction typo

--- src/components/JeuListComponent.tsx
+++ src/components/JeuListComponent.tsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import "../assets/JeuList.css"
+import { useState, useEffect } from 'react'
 import Jeu from '../models/Jeu'
-import { Link } from "react-router-dom";
-import axios from "axios";
+import { Link } from "react-router-dom"
+import axios from "axios"
 
 export default function JeuListComponent() {
 
-    const [typesJeu,setTypeJeu] = useState([])
+    const [typesJeu,setTypesJeu] = useState([])
     let headersList = {
         Accept: "*/*",
         Autorization: localStorage.getItem("token"),
@@ -15,11 +16,11 @@ export default function JeuListComponent() {
         let reqOptions = {
             url: "http://localhost:3000/jeux/",
             method: "get",
-          };
+        };
         
         axios(reqOptions).then(function (response) {
-            console.log(response.data);
-            setTypeJeu(response.data);
+            console.log(response.data)
+            setTypesJeu(response.data)
         });
     },[]); 
 
@@ -28,10 +29,9 @@ export default function JeuListComponent() {
             <h1>Liste des types de jeu</h1>
             <div id="test">
                 {typesJeu.map((item : Jeu) => 
-                    <Link to={{pathname : String(item.idJeu)}} state = {{item}} className='link'>{item.idJeu + ' : ' + item.nom}</Link>
+                    <Link to={{pathname : String(item.idJeu)}} state = {{item}} className='lien-jeu'>{item.idJeu + ' : ' + item.nom}</Link>
                 )}
             </div>
         </div>
     )
 }
-
