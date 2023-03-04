24ef5222e8d2b16f8501860f0e8db3f704ba44ee
EAgnese <agneseeri@gmail.com>
2023-03-04 16:56:33 +0100

    affichage des créneau plus clean

--- src/components/Creneau.tsx
+++ src/components/Creneau.tsx
@@ -1,30 +1,53 @@
 import Paper from '@mui/material/Paper';
-import { AppointmentModel, ViewState } from '@devexpress/dx-react-scheduler';
+import { AppointmentModel, ViewState, EditingState, IntegratedEditing, ChangeSet} from '@devexpress/dx-react-scheduler';
 import {
 	Scheduler,
 	WeekView,
 	Appointments,
+	AppointmentForm,
+	AppointmentTooltip,
 	Toolbar,
 	DateNavigator,
 	TodayButton,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import { useState, useEffect } from 'react';
 import axios from "axios"
+import { getToken, isAdmin, isConnected} from '../middleware/token'
+import { indigo, blue, teal  } from '@mui/material/colors';
 
-export default function CreneauComponent() {
+const currentDate = Date.now();
 
-	const currentDate = Date.now();
+const AppointmentContent = (({data, ...restProps}: Appointments.AppointmentContentProps) =>  {
+	return (
+	  	<Appointments.AppointmentContent data={data}>
+			<div>
+		  		<div>
+					{data.title}
+				</div>
+				<div>
+					{`Zone: ${data.zone}`}
+				</div>
+				<div>
+					{`Benevole: ${data.benevole}`}
+				</div>
+			</div>
+		</Appointments.AppointmentContent>
+	);
+});
+
+export default function CreneauComponent() {	
 
 	const [schedulerData,setSchedulerData] = useState<AppointmentModel[]>()
 	let headersList = {
-		Accept: "*/*",
-		Autorization: '',
+		Accept: "*",
+		Autorization: 'Bearer '+getToken()?.toString(),
 	};
 
 	useEffect(() => {  
 		let reqOptions = {
 			url: "http://localhost:3000/attributions/",
 			method: "get",
+			headers: headersList
 		};
 		
 		axios(reqOptions).then(function (response) {
@@ -36,7 +59,9 @@ export default function CreneauComponent() {
 				let temp = {
 					startDate : att.dateDebut,
 					endDate : att.dateFin,
-					title : att.nomZone + " avec " + att.nom
+					title : att.nomZone + " avec " + att.nom,
+					zone : att.nomZone,
+					benevole : att.nom
 				}
 				result.push(temp)
 			}
@@ -48,14 +73,25 @@ export default function CreneauComponent() {
 		<Paper>
 			<Scheduler data={schedulerData}>	
 				<ViewState defaultCurrentDate={currentDate} />
+				<EditingState onCommitChanges={function (changes: ChangeSet): void {
+					throw new Error('Function not implemented.');
+				} }/>
+				<IntegratedEditing />
 				<WeekView
 					startDayHour={9}
 					endDayHour={19}
 				/>	
-				<Appointments />
+				<Appointments 
+					appointmentContentComponent={AppointmentContent}
+				/>
 				<Toolbar />
           		<DateNavigator />
 				<TodayButton />
+				<AppointmentTooltip
+					showOpenButton
+					showDeleteButton
+				/>
+				<AppointmentForm />
 			</Scheduler>
 		</Paper>
 	);
